31940d4abb988047e3e272fea41977e82f7b0321
Yahya Batulu <yahyabatulu00@gmail.com>
2023-12-16 19:09:06 +0300

    Localizer singleton

--- src/Localizer.php
+++ src/Localizer.php
@@ -2,14 +2,29 @@
 
 namespace ByPikod\Localization;
 
+/**
+ * Singleton class for localization
+ */
 class Localizer
 {
     protected $translation;
+    protected $translations = [];
     protected $app;
 
-    public function __construct(Translation $translation, $app)
+    public function __construct($app, Translation $translation)
     {
-        $this->translation = $translation;
         $this->app = $app;
+        $this->translation = $translation;
+    }
+
+    public function fetchTranslation($name, $locale)
+    {
+        $this->translations[$locale][$name] = "fetched translation";
+    }
+
+    public function translate($name, $locale = null)
+    {
+        $locale = $locale ?: $this->app->getLocale();
+        return $this->translations[$locale][$name] ?? "undefined";
     }
 }
